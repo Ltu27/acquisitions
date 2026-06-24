e6a5863187e29d4e4fc897176ea6c32f981f1ce4
leba.vietanh <leba.vietanh.scsoft@gmail.com>
2026-06-24 11:42:50 +0700

    Bỏ qua cảnh báo console log bằng comment inline

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}`);
 });
